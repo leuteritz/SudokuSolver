97644c1442061f8764d6a5d93aefde466506ad27
Adrian Leuteritz <adrianleuteritz@web.de>
2024-03-31 00:07:42 +0100

    list of numbers + change with mouse click

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 import pygame
 
 WINDOW_HEIGHT = 594
 WINDOW_WIDTH = 594
+SIZE = WINDOW_HEIGHT//9
 
 # pygame setup
 pygame.init()
@@ -14,17 +14,32 @@ number = ''
 # font
 font = pygame.font.Font(None, 36)
 
+list = [['1', '9', '3', '1', '2', '3', '1', '2', '3'],
+        ['2', '8', '3', '1', '2', '3', '1', '2', '3'],
+        ['3', '7', '3', '1', '2', '3', '1', '2', '3'],
+        ['4', '6', '3', '1', '2', '3', '1', '2', '3'],
+        ['5', '5', '3', '1', '2', '3', '1', '2', '3'],
+        ['6', '4', '3', '1', '2', '3', '1', '2', '3'],
+        ['7', '3', '3', '1', '2', '3', '1', '2', '3'],
+        ['8', '2', '3', '1', '2', '3', '1', '2', '3'],
+        ['9', '1', '3', '1', '2', '3', '1', '2', '3']]
+
 
 def drawGrid():
-    size = WINDOW_HEIGHT//9
-    for i in range(0, WINDOW_WIDTH, size):
-        for j in range(0, WINDOW_HEIGHT, size):
-            rect = pygame.Rect(i, j, size, size)
+    a = 0
+    b = 0
+    for i in range(0, WINDOW_WIDTH, SIZE):
+        b = 0  # Reset column index for each row
+        for j in range(0, WINDOW_HEIGHT, SIZE):
+            rect = pygame.Rect(i, j, SIZE, SIZE)
 
             pygame.draw.rect(screen, 'white', rect, 1, 10)
-            number_text = font.render(number, True, 'white')
+            number_text = font.render(list[a][b], True, 'white')
             screen.blit(
                 number_text, (rect.x + (rect.w - number_text.get_width())/2, rect.y + (rect.h - number_text.get_height())/2))
+            b += 1
+
+        a += 1
 
 
 while running:
@@ -33,9 +48,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.unicode.isdigit():
-                number += event.unicode
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            print(mouse_pos)
+            clicked_row = mouse_pos[1] // SIZE
+            clicked_col = mouse_pos[0] // SIZE
+            list[clicked_col][clicked_row] = '4'
 
     screen.fill("black")
     drawGrid()
