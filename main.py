2989747f93caf485e275fa192895df11d9a4a725
Adrian Leuteritz <adrianleuteritz@web.de>
2024-03-31 01:09:33 +0100

    change color

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ SIZE = WINDOW_HEIGHT//9
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+pygame.display.set_caption("Sudoku")
+
 clock = pygame.time.Clock()
 running = True
 number = ''
@@ -33,13 +35,32 @@ def drawGrid():
         for j in range(0, WINDOW_HEIGHT, SIZE):
             rect = pygame.Rect(i, j, SIZE, SIZE)
 
-            pygame.draw.rect(screen, 'white', rect, 1, 10)
-            number_text = font.render(list[a][b], True, 'white')
+            pygame.draw.rect(screen, 'gray', rect, 2)
+            number_text = font.render(list[a][b], True, 'black')
             screen.blit(
                 number_text, (rect.x + (rect.w - number_text.get_width())/2, rect.y + (rect.h - number_text.get_height())/2))
+
+            # draw game corner border
+            if a == 3 or a == 6:
+                pygame.draw.rect(screen, 'black', (i - 5, j, 10, SIZE))
+            if b == 3 or b == 6:
+                pygame.draw.rect(screen, 'black', (i, j - 5, SIZE, 10))
+
+            # draw a green border in selected cell
+            if selected_cell is not None and selected_cell == (b, a):
+
+                pygame.draw.rect(screen, 'green', rect, 5)
+
             b += 1
 
         a += 1
+
+
+def updateGrid(row, col, number):
+    list[col][row] = number
+
+
+selected_cell = None
 
 
 while running:
@@ -50,12 +71,18 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            print(mouse_pos)
+
             clicked_row = mouse_pos[1] // SIZE
             clicked_col = mouse_pos[0] // SIZE
-            list[clicked_col][clicked_row] = '4'
+            selected_cell = (clicked_row, clicked_col)
 
-    screen.fill("black")
+        elif event.type == pygame.KEYDOWN:
+
+            if pygame.K_1 <= event.key <= pygame.K_9:
+                number = chr(event.key)
+                updateGrid(clicked_row, clicked_col, number)
+
+    screen.fill("white")
     drawGrid()
 
     pygame.display.flip()
